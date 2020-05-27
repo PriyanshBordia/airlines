f372f2b8e524a964ae66a68d5492def64f108fc4
Priyansh Bordia <priyanshbordia1@gmail.com>
2020-05-27 12:31:48 +0530

    changed files

--- flights/admin.py
+++ flights/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import Airport, Flight, Passenger
 
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra = 1
+
+class FligthAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    flig
+
 # Register your models here.
 admin.site.register(Airport)
 admin.site.register(Flight)
